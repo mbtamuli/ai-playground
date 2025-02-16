cf4f08f84de8234b0a134900d4a6d6bba3b2c9ff
Mriyam Tamuli <mbtamuli@gmail.com>
2025-02-16 17:21:42 +0530

    Trying slider with a container
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- snake/src/ui/game_screen.go
+++ snake/src/ui/game_screen.go
@@ -5,7 +5,7 @@ import (
 	"log"
 
 	"github.com/ebitenui/ebitenui"
-	"github.com/ebitenui/ebitenui/image"
+	eimage "github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 )
 
@@ -41,15 +41,6 @@ func createGameScreen(manager *UIManager) *ebitenui.UI {
 		})),
 	)
 
-	// Add sliders for AI settings
-	//explorationSlider := widget.NewSlider(
-	//	widget.SliderOpts.Value(g.gameManager.ExplorationRate),
-	//	widget.SliderOpts.Range(0, 1),
-	//	widget.SliderOpts.OnChanged(func(args *widget.SliderChangedEventArgs) {
-	//		g.gameManager.UpdateExplorationRate(args.Value)
-	//	}),
-	//)
-
 	sliderContainer := widget.NewContainer(
 		widget.ContainerOpts.Layout(widget.NewStackedLayout(
 			widget.StackedLayoutOpts.Padding(widget.Insets{
@@ -60,22 +51,10 @@ func createGameScreen(manager *UIManager) *ebitenui.UI {
 
 	trackGraphic := widget.NewGraphic(
 		widget.GraphicOpts.Image(manager.SliderResources.TrackImage),
-		widget.GraphicOpts.WidgetOpts(widget.WidgetOpts.Dropped(
-			func(args *widget.DragAndDropDroppedEventArgs) {
-				log.Println("Dropped")
-				log.Println(args)
-			},
-		)),
 	)
 
 	handleGraphic := widget.NewGraphic(
 		widget.GraphicOpts.Image(manager.SliderResources.HandleImage),
-		widget.GraphicOpts.WidgetOpts(widget.WidgetOpts.Dropped(
-			func(args *widget.DragAndDropDroppedEventArgs) {
-				log.Println("Dropped")
-				log.Println(args)
-			},
-		)),
 	)
 
 	explorationSlider := widget.NewSlider(
@@ -84,8 +63,8 @@ func createGameScreen(manager *UIManager) *ebitenui.UI {
 		}), widget.WidgetOpts.MinSize(200, 6)),
 		widget.SliderOpts.MinMax(1, 10),
 		widget.SliderOpts.Images(&widget.SliderTrackImage{}, &widget.ButtonImage{
-			Idle:    image.NewNineSliceColor(color.RGBA{255, 255, 255, 255}),
-			Pressed: image.NewNineSliceColor(color.RGBA{255, 255, 255, 255}),
+			Idle:    eimage.NewNineSliceColor(color.RGBA{255, 255, 255, 255}),
+			Pressed: eimage.NewNineSliceColor(color.RGBA{255, 255, 255, 255}),
 		}),
 		widget.SliderOpts.FixedHandleSize(10),
 		widget.SliderOpts.TrackOffset(5),
@@ -94,6 +73,10 @@ func createGameScreen(manager *UIManager) *ebitenui.UI {
 		}),
 		widget.SliderOpts.ChangedHandler(func(args *widget.SliderChangedEventArgs) {
 			log.Println(args.Current)
+			gld := settingsPanel.GetWidget().LayoutData.(widget.GridLayoutData)
+			gld.HorizontalPosition = widget.GridLayoutPositionStart
+			settingsPanel.GetWidget().LayoutData = gld
+			settingsPanel.RequestRelayout()
 		}),
 	)
 
@@ -101,25 +84,7 @@ func createGameScreen(manager *UIManager) *ebitenui.UI {
 	sliderContainer.AddChild(trackGraphic)
 	sliderContainer.AddChild(explorationSlider)
 
-	//riskSlider := widget.NewSlider(
-	//	widget.SliderOpts.Value(g.gameManager.RiskAppetite),
-	//	widget.SliderOpts.Range(0, 1),
-	//	widget.SliderOpts.OnChanged(func(args *widget.SliderChangedEventArgs) {
-	//		g.gameManager.UpdateRiskAppetite(args.Value)
-	//	}),
-	//)
-
-	//learningSlider := widget.NewSlider(
-	//	widget.SliderOpts.Value(g.gameManager.LearningRate),
-	//	widget.SliderOpts.Range(0, 1),
-	//	widget.SliderOpts.OnChanged(func(args *widget.SliderChangedEventArgs) {
-	//		g.gameManager.UpdateLearningRate(args.Value)
-	//	}),
-	//)
-
 	settingsPanel.AddChild(sliderContainer)
-	//settingsPanel.AddChild(riskSlider)
-	//settingsPanel.AddChild(learningSlider)
 
 	rootContainer.AddChild(gameView)
 	rootContainer.AddChild(settingsPanel)
